4147a144a2f4b56af995b40b9c6e3f05b6461a46
Tatiana Vega <tatianavegac2003@gmail.com>
2026-03-18 12:03:23 -0500

    Agregar configuración de CI, archivos de aplicación y pruebas

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log("Aplicación corriendo correctamente");
---
+++ sum.test.js
@@ -0,0 +1,8 @@
+const sum = require("./sum");
+
+if (sum(2, 2) !== 4) {
+  console.error("Test falló");
+  process.exit(1);
+}
+
+console.log("Test pasó");
